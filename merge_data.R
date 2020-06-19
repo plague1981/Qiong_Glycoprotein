366bce60a9116cb89aa2ffb4393c972f8ca8c01d
plague1981 <42053196+plague1981@users.noreply.github.com>
2020-06-19 15:07:35 -0400

    for data merge in app.R

---
+++ merge_data.R
@@ -0,0 +1,10 @@
+merge.data<-function(filetype,file1,file2,file3){
+  if (filetype=='csv'){
+    csv.file.table<-NULL
+    for (f in c(file1,file2,file3)){
+      csv.file.table$f<-data.frame(read.csv(file = f, stringsAsFactors = FALSE)[,c(1,2,8,7,20)])
+    }
+    csv.file.merge.table<-rbind(csv.file.table$file1,csv.file.table$file2,csv.file.table$file3)
+    return(csv.file.merge.table)
+  }
+}
